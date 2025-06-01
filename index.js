2d500baa2ad670cf2f37610691c7ec0e823be9a8
smabari <mdazizulbarishafin@gmail.com>
2025-06-01 12:25:50 +0600

    fix: complete job applications API endpoint to return results based on applicant email

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ async function run() {
     // job applications api
     app.get("/applications", async (req, res) => {
       const email = req.query.email;
-      
+      const query = { applicant: email };
+      const result = await applicationsCollection.find(query).toArray();
+      res.send(result);
     });
 
     app.post("/applications", async (req, res) => {
