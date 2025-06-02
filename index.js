b42470f7afc87638348f0c5e56d01c1783a53691
smabari <mdazizulbarishafin@gmail.com>
2025-06-03 03:44:47 +0600

    fix: implement email filtering in jobs endpoint query

--- index.js
+++ index.js
@@ -33,18 +33,24 @@ async function run() {
 
     // jobs api
     app.get("/jobs", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.hr_email = email;
+      }
+
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
     // could be done, but should not be done
-    app.get("/jobsByEmailAddress", async (req, res) => {
-      const email = req.query.email;
-      const query = { hr_email: email };
-      const result = await jobsCollection.find(query);
-      res.send(result);
-    });
+    // app.get("/jobsByEmailAddress", async (req, res) => {
+    //   const email = req.query.email;
+    //   const query = { hr_email: email };
+    //   const result = await jobsCollection.find(query);
+    //   res.send(result);
+    // });
 
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
