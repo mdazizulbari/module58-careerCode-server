050e3da9a4890b9e8ee5819ab5b57547fb91fc39
smabari <mdazizulbarishafin@gmail.com>
2025-06-02 20:28:53 +0600

    feat: add GET endpoint for retrieving jobs by email address

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ async function run() {
       res.send(result);
     });
 
+    // could be done
+    app.get("/jobsByEmailAddress", async (req, res) => {
+      const email = req.query.email;
+      const query = { hr_email: email };
+    });
+
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
